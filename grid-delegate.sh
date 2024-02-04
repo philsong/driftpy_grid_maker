72faf934503efedf5c182758127f7f82bf57ee17
EC2 Default User <ec2-user@ip-172-31-35-113.ap-northeast-1.compute.internal>
2024-02-04 05:29:37 +0000

    fix delegate

--- grid-delegate.sh
+++ grid-delegate.sh
@@ -1,2 +1,2 @@
 export ANCHOR_WALLET="/home/ec2-user/drift/delegate-key.json"
-python limit_order_grid.py --env mainnet --market SOL-PERP --authority 9tAoVCc48VrezYTwau1AcZ3LPfjMDU5JaugAikgsePFW  --min-position -220 --max-position -120 --loop 10 --amount 200 --grids 5 --spread 0.005 --target-pos -170
+python limit_order_grid.py --env mainnet --market SOL-PERP --authority 9tAoVCc48VrezYTwau1AcZ3LPfjMDU5JaugAikgsePFW  --min-position -250 --max-position -150 --loop 10 --amount 400 --grids 5 --spread 0.005 --target-pos -200
