494492d81fa678225749ff92683c6c4861ba7e20
EC2 Default User <ec2-user@ip-172-31-35-113.ap-northeast-1.compute.internal>
2024-02-17 02:44:13 +0000

    update sh

--- grid-delegate.sh
+++ grid-delegate.sh
@@ -1,2 +1,2 @@
 export ANCHOR_WALLET="/home/ec2-user/drift/delegate-key.json"
-python limit_order_grid.py --env mainnet --market SOL-PERP --authority 9tAoVCc48VrezYTwau1AcZ3LPfjMDU5JaugAikgsePFW  --min-position -300 --max-position 100 --loop 10 --amount 400 --grids 5 --spread 0.005 --target-pos -100
+python limit_order_grid.py --env mainnet --market SOL-PERP --authority 9tAoVCc48VrezYTwau1AcZ3LPfjMDU5JaugAikgsePFW  --min-position -200 --max-position 200 --loop 5 --amount 400 --grids 5 --spread 0.005 --target-pos 0
--- grid-delegate2-eth.sh
+++ grid-delegate2-eth.sh
@@ -1,2 +1,2 @@
 export ANCHOR_WALLET="/home/ec2-user/drift/delegate-key2.json"
-python limit_order_grid.py --subaccount 1 --authority 3YsLJNp2pjxr1yc9j6b4Bm9cBePVCgVdw7Ky2DzymhJk --env mainnet --market ETH-PERP --loop 10  --target-pos 0 --min-position -20 --max-position 20 --amount 400 --grids 5 --spread 0.01
+python limit_order_grid.py --subaccount 1 --authority 3YsLJNp2pjxr1yc9j6b4Bm9cBePVCgVdw7Ky2DzymhJk --env mainnet --market ETH-PERP --loop 5  --target-pos 0 --min-position -20 --max-position 20 --amount 400 --grids 5 --spread 0.01
