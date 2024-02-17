5b1c4248a1c659f6688398e609fe5868b8aad53a
EC2 Default User <ec2-user@ip-172-31-36-6.ap-northeast-1.compute.internal>
2024-02-17 02:47:18 +0000

    sh

--- grid.sh
+++ grid.sh
@@ -1,2 +1,2 @@
 export ANCHOR_WALLET="/home/ec2-user/drift/my-keypair.json"
-python limit_order_grid.py --env mainnet --market SOL-PERP  --min-position -300 --max-position -100 --loop 10 --amount 400 --grids 5 --spread 0.005 --target-pos -200
+python limit_order_grid.py --env mainnet --market SOL-PERP  --min-position -300 --max-position -100 --loop 5 --amount 400 --grids 5 --spread 0.005 --target-pos -200
