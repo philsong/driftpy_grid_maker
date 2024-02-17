05aed25916f6b8167e336d3ddc01172a1e0234db
EC2 Default User <ec2-user@ip-172-31-46-50.ap-northeast-1.compute.internal>
2024-02-17 02:45:26 +0000

    sh

--- shi-grid.sh
+++ shi-grid.sh
@@ -1,2 +1,2 @@
 export ANCHOR_WALLET="/home/ec2-user/.shiwallet.json"
-python limit_order_grid.py --env mainnet --market SOL-PERP  --min-position -50 --max-position 50 --loop 10 --amount 100 --grids 5 --spread 0.005 --target-pos 0
+python limit_order_grid.py --env mainnet --market SOL-PERP  --min-position -50 --max-position 50 --loop 5 --amount 100 --grids 5 --spread 0.005 --target-pos 0
